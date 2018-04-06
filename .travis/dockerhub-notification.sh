e1c68314aa1e240e91e2182908c21c4208cc3026
Nick Lanng <rinse@rinseworld.com>
2018-04-06 10:50:54 +0100

    Added DockerHub deploy notification (#1)

---
+++ .travis/dockerhub-notification.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+ret=$?
+if [ $ret -eq 0 ]; then
+  STATE="success"
+else
+  STATE="failure"
+fi
+
+echo -e "[Webhook]: Sending webhook to Discord...\\n";
+
+IMAGE=$1
+
+case $STATE in
+  "success" )
+    EMBED_COLOR=3066993
+    STATUS_MESSAGE="Pushed"
+    ;;
+
+  "failure" )
+    EMBED_COLOR=15158332
+    STATUS_MESSAGE="Failed"
+    ;;
+
+  * )
+    EMBED_COLOR=0
+    STATUS_MESSAGE="Status Unknown"
+    ;;
+esac
+
+
+if [ "$AUTHOR_NAME" == "$COMMITTER_NAME" ]; then
+  CREDITS="$AUTHOR_NAME authored & committed"
+else
+  CREDITS="$AUTHOR_NAME authored & $COMMITTER_NAME committed"
+fi
+
+TIMESTAMP=$(date --utc +%FT%TZ)
+WEBHOOK_DATA='{
+  "username": "",
+  "avatar_url": "https://cdn.iconscout.com/public/images/icon/free/png-512/docker-logo-35698963e9c1a96c-512x512.png",
+  "embeds": [ {
+    "color": '$EMBED_COLOR',
+    "author": {
+      "name": "'"$IMAGE"'",
+      "url": "https://hub.docker.com/r/'"$TRAVIS_REPO_SLUG"'/tags/"
+    },
+    "title": "'"$COMMIT_SUBJECT"'",
+    "url": "'"$URL"'",
+    "description": "'"$STATUS_MESSAGE"'",
+    "fields": [],
+    "timestamp": "'"$TIMESTAMP"'"
+  } ]
+}'
+
+(curl --fail --progress-bar -A "Dockerhub-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "$WEBHOOK_DATA" "$2" \
+  && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
